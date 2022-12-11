b09fa2df5a6384e4e4d7513b147bf1445017daa8
SnakeVeryLong <ermolenko220699@gmail.com>
2022-12-11 18:58:19 +0700

    now

--- Assets/Scripts/UserPlay.cs
+++ Assets/Scripts/UserPlay.cs
@@ -7,7 +7,15 @@ public class UserPlay : MonoBehaviour
     void Update()
     {
         Vector3 pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+          this.transform.Translate(pos.x * Time.deltaTime, pos.y * Time.deltaTime, 0, Camera.main.transform);
+ Vector3 pos2 = new Vector3(this.transform.position.x, pos.y, 0);
+
+      //  var angle = Mathf.Atan2((pos - transform.position).y, (pos - transform.position).x) * Mathf.Rad2Deg;
+      //  rb.AddForce(-90f, 0, angle);
+       //this.transform.rotation = Quaternion.Euler(-90f, 0, -angle);
+         // Debug.Log(Quaternion.Angle(Quaternion.Euler(pos2), Quaternion.Euler(transform.position)));
+        
+      
 
         //float hor = Input.GetAxisRaw("Horizontal");
 
